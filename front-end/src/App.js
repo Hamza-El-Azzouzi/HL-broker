590a6149b73bf240b8fc53671d7988a3ef72a931
Hamza El Azzouzi <elazzouzi.hamza20@gmail.com>
2023-05-04 16:45:22 +0100

    Add spinner && Exception page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,35 +1,53 @@
 
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route , Redirect } from 'react-router-dom'
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+
 
 import NavBar from './includes/NavBar';
 import Footer from './includes/Footer';
 import HomePage from './HomePage';
 import HomeVendeur from './Vendeur/HomeVendeur';
+import NotFound from './Exception/Comp/404';
+import { useState ,useEffect } from 'react';
+import NotAuthorized from './Exception/Comp/403';
 
 
 
 export default function App() {
 
+  const [isLoggedIn, setIsLoggedIn] = useState({
+    type : 'acheteur'
+  });
 
-
+  useEffect(() => {
+    // Simulate authentication
+    setIsLoggedIn({ type : 'vendeur'}); // Change this to false to simulate a 403 error
+  }, []);
   window.addEventListener("scroll", function () {
     const search = document.querySelector(".navbar")
     search.classList.toggle("active", window.scrollY > 100)
   })
 
   return (
+
     <div className="App">
+
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <NavBar />
       </nav>
       <>
         <BrowserRouter>
           <Switch>
-            <Route path='/Home' index>{<HomePage/>}</Route>
-            <Route path='/HomeVendeur'>{<HomeVendeur />}</Route>
+            <Route exact path='/' component={HomePage} index />
+            {isLoggedIn.type === 'vendeur' ? ( <Route path='/HomeVendeur' component={HomeVendeur} />):
+            (
+              <NotAuthorized/>
+            ) }
+           
+           <Route path="/NotFound" component={NotFound} />
+            <Redirect to="/NotFound" />
           </Switch>
         </BrowserRouter>
 
@@ -41,7 +59,10 @@ export default function App() {
       </footer>
 
     </div>
-  );
+
+  )
 }
+
+
 
 
--- front-end/src/Vendeur/HomeVendeur.js
+++ front-end/src/Vendeur/HomeVendeur.js
@@ -1,11 +1,12 @@
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Link ,Redirect } from 'react-router-dom';
 import './HomeVendeur.css'
 import ShowArticle from "./comp/ShowArticle";
 import AddArticle from './comp/AddArticle';
-import { } from 'react-router-dom/cjs/react-router-dom.min';
+
 import Dashboard from './comp/Dashboard';
 import UpadateArticle from './comp/UpdateArticle';
 import ShowDemande from './comp/ShowDemande';
+
 
 
 
@@ -17,7 +18,7 @@ export default function HomeVendeur() {
             <div className='VendurPage'>
                 <div className='VendeurNav'>
                     <div className='box f_flex'>
-                        <Link to='/HomeVendeur/Dashboard'> <span>Dashboard</span></Link>
+                        <Link to='/HomeVendeur'> <span>Dashboard</span></Link>
                     </div>
                     <div className='box f_flex'>
                         <Link to='/HomeVendeur/Demande'> <span>Demande</span></Link>
@@ -29,11 +30,13 @@ export default function HomeVendeur() {
 
                 <Switch>
                    
-                    <Route path='/HomeVendeur/Dashboard' exact index> <Dashboard/></Route>
-                    <Route path='/HomeVendeur/Article'> <ShowArticle /></Route>
-                    <Route path='/HomeVendeur/AddArticle'> <AddArticle /></Route>
-                    <Route path='/HomeVendeur/UpadateArticle'><UpadateArticle/> </Route>
-                    <Route path='/HomeVendeur/Demande'> <ShowDemande/> </Route>
+                    <Route path='/HomeVendeur' exact index  component={Dashboard} /> 
+                    <Route path='/HomeVendeur/Article' component={ShowArticle} /> 
+                    <Route path='/HomeVendeur/AddArticle' component={AddArticle} /> 
+                    <Route path='/HomeVendeur/UpadateArticle' component={UpadateArticle} />
+                    <Route path='/HomeVendeur/Demande' component={ShowDemande}/ > 
+                    <Route path="/NotFound" />
+                    <Redirect to="/NotFound" />
                 </Switch>
             </div>
         </BrowserRouter>
